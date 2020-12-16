0f698f6438042a983243e9e8b507d3e7bdaa7c6e
Anas Alkhatib <anas.alkhatib@shopify.com>
2020-12-15 21:24:18 -0500

    solves part 2 with rubocop and some improvements

--- src/year2020/day15.rb
+++ src/year2020/day15.rb
@@ -1,34 +1,26 @@
 module Year2020
   class Day15
-
-    def part1(input, limit=2020)
+    def part1(input, limit = 2020)
       spoken = input.split(',').map(&:to_i)
-      current_index = nil
-      spoken_dict = {}
-      spoken.each_with_index {|n, i| spoken_dict[n] = i; current_index = i}
-      current_index += 1
+      spoken_dict = spoken.each_with_index.to_h
+      current_index = spoken_dict.values.last + 1
       next_spoken = 0
-      while current_index < limit - 1
-        puts current_index  if current_index % 1000 == 0
-
-
-        if spoken_dict.include? next_spoken
-          next_spoken_index = spoken_dict[next_spoken]
-          spoken_dict.delete(next_spoken)
-          spoken_dict[next_spoken] = current_index
-          next_spoken = current_index - next_spoken_index
-          current_index += 1
-        else
-          spoken_dict[next_spoken] = current_index
-          next_spoken = 0
-          current_index += 1
-        end
+      (current_index...limit - 1).each do
+        print '.' if (current_index % 100_000).zero?
+        next_spoken_index = spoken_dict.delete(next_spoken)
+        spoken_dict[next_spoken] = current_index
+        next_spoken = if next_spoken_index.nil?
+                        0
+                      else
+                        current_index - next_spoken_index
+                      end
+        current_index += 1
       end
       next_spoken
     end
 
     def part2(input)
-      part1(input, 30000000)
+      part1(input, 30_000_000)
     end
   end
 end
